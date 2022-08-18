d51262de45b5baf17ca73a4701fe4fd88117e347
Daniil <dtuneev@gmail.com>
2022-08-18 03:21:24 +0300

    input and button components + icons

---
+++ src/components/ui/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let text;
+</script>
+
+<button class='h-12 my-4 text-white w100 bg-gray-800 flex items-center justify-center rounded-full'>
+	{text}
+</button>
---
+++ src/components/ui/Input.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let placeholder;
+	export let id;
+	export let type = 'text';
+</script>
+
+<label for={id}>
+	<input type={type} id={id} class="border-2 border-solid border-gray-800 my-4 text-gray-800 text-sm rounded-lg
+				block w-full p-2.5 not-italic" placeholder={placeholder} required>
+</label>
